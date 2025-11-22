a8064f2b32315cbd1e9e04d552d5371fe732952e
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-11-22 14:46:37 +0100

    fixed layout's font

--- lespattesheureuses/resources/views/components/client/global/layout.blade.php
+++ lespattesheureuses/resources/views/components/client/global/layout.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="overflow-x-hidden max-w-[1400px] mx-auto">
+<body class="overflow-x-hidden max-w-[1400px] mx-auto font-poppins">
 <x-client.global.nav/>
 <main class="w-[calc(100%-32px*2)] md:w-[calc(100%-48*2)] lg:w-[calc(100%-64px*2)] mx-auto">
     {{$slot}}
