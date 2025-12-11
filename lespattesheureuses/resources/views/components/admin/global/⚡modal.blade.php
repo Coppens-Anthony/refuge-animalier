cec09cad0b8d3de4e8cb7c07499d95d66eac360b
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-12-11 11:13:39 +0100

    fixed modal's width

--- lespattesheureuses/resources/views/components/admin/global/⚡modal.blade.php
+++ lespattesheureuses/resources/views/components/admin/global/⚡modal.blade.php
@@ -9,6 +9,6 @@ new class extends Component
 ?>
 
 
-<div x-show="open" class="p-6 fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform origin-center bg-white border-primary border-2 rounded-2xl shadow-2xl backdrop:bg-black backdrop:opacity-50">
+<div x-show="open" class="p-6 fixed w-[50vw] z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform origin-center bg-white border-primary border-2 rounded-2xl shadow-2xl backdrop:bg-black backdrop:opacity-50">
     {{$slot}}
 </div>
