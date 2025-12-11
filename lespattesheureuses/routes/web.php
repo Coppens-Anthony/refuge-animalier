1fb8b3abe7bc0c94fd4c6874edfc921de8ddb5d8
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-12-11 14:49:35 +0100

    fixed animals create route

--- lespattesheureuses/routes/web.php
+++ lespattesheureuses/routes/web.php
@@ -14,10 +14,10 @@ Route::domain('admin.lespattesheureuses.test')->group(function () {
 
     Route::livewire('/animals', 'pages::animals.⚡index')
         ->name('index.animals')->middleware('auth');
-    Route::livewire('/animals/{animal}', 'pages::animals.⚡show')
-        ->name('show.animals')->middleware('auth');
     Route::livewire('/animals/create', 'pages::animals.⚡create')
         ->name('create.animals')->middleware('auth');
+    Route::livewire('/animals/{animal}', 'pages::animals.⚡show')
+        ->name('show.animals')->middleware('auth');
 
     Route::livewire('/requests', 'pages::requests.⚡index')
         ->name('index.requests')->middleware('auth');
