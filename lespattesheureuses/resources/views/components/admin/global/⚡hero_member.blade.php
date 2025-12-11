fcf73bcee16b05be19b25e79e9e07190c4b36149
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-12-11 14:26:47 +0100

    injecting member's data in his show page

--- lespattesheureuses/resources/views/components/admin/global/⚡hero_member.blade.php
+++ lespattesheureuses/resources/views/components/admin/global/⚡hero_member.blade.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Models\User;
 use Livewire\Component;
 
 new class extends Component {
-    //
+    public User $member;
+    public array $member_datas;
+
+
+    public function mount(User $member): void
+    {
+        $this->member_datas = [
+            'avatar' => $this->member->avatar,
+            'name' => $this->member->name,
+            'email' => $this->member->email,
+            'telephone' => $this->member->telephone,
+            'status' => $this->member->status,
+        ];
+    }
 };
 ?>
 
@@ -11,21 +25,21 @@ new class extends Component {
     <section class="flex gap-30 items-center">
         <div class="flex flex-col gap-8 w-1/2">
             <div class="flex gap-4 items-center">
-                <h3 class="text-2xl">John Doe</h3>
+                <h3 class="text-2xl">{{$this->member->name}}</h3>
                 <x-client.global.status
                     isInCard="{{false}}"
                 >
-                    {!! __('admin/global.volunteer') !!}
+                    {{$this->member->status}}
                 </x-client.global.status>
             </div>
             <div class="flex flex-col gap-2">
                 <div class="flex gap-2 items-center">
                     <img src="{{asset('assets/icons/email.svg')}}" alt="{!! __('global.email_icon') !!}">
-                    <a href="mailto:lespattesheureuse@gmail.com" {!! __('global.email_title') !!} class="link">lespattesheureuse@gmail.com</a>
+                    <a href="mailto:{{$this->member->email}}" {!! __('global.email_title') !!} class="link">{{$this->member->email}}</a>
                 </div>
                 <div class="flex gap-2 items-center">
                     <img src="{{asset('assets/icons/telephone.svg')}}" alt="{!! __('global.telephone_icon') !!}">
-                    <a href="tel:0123.45.67.89" {!! __('global.telephone_title') !!} class="link">0123.45.67.89</a>
+                    <a href="tel:{{$this->member->telephone}}" {!! __('global.telephone_title') !!} class="link">{{$this->member->telephone}}</a>
                 </div>
             </div>
         </div>
--- lespattesheureuses/resources/views/pages/members/⚡show.blade.php
+++ lespattesheureuses/resources/views/pages/members/⚡show.blade.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Models\User;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
-new #[Title('Profil de John Doe')]
+new #[Title('Profil de ')]
 class extends Component {
+    public User $member;
+
 };
 ?>
-
 <div class="flex flex-col gap-8">
-    <livewire:admin.global.hero_member/>
+    <livewire:admin.global.hero_member
+        :member="$this->member"
+    />
     <livewire:admin.global.dispo
         dispo_title="{{__('admin/dispo.his_dispo')}}"
     />
