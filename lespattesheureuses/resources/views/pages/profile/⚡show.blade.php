6e5ed5ade931d635de63107409aab7393af467c3
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2025-12-13 10:35:45 +0100

    fixed profile with the auth user

--- lespattesheureuses/resources/views/pages/profile/⚡show.blade.php
+++ lespattesheureuses/resources/views/pages/profile/⚡show.blade.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Models\User;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 new #[Title('Mon profil')]
 class extends Component {
+    #[Computed]
+    public function authUser()
+    {
+        return auth()->user();
+    }
+
 };
 ?>
 
 <div class="flex flex-col gap-8">
-    <livewire:admin.global.hero_member/>
+    <livewire:admin.global.hero_member
+        :member="$this->authUser"
+    />
     <livewire:admin.global.dispo
         dispo_title="{{__('admin/dispo.my_dispo')}}"
     />
